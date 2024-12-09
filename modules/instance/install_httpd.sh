484f17714a329e1e1394be87bde0809d401d9ccf
fu-haojie <fu-haojie@fu-haojies-MacBook-Pro.local>
2024-12-09 00:19:55 -0500

    modify web page to download image from s3

--- modules/instance/install_httpd.sh
+++ modules/instance/install_httpd.sh
@@ -2,6 +2,9 @@
 sudo yum -y update
 sudo yum -y install httpd
 myip=`curl http://169.254.169.254/latest/meta-data/local-ipv4`
-echo "<h1>Haojie Fu, Welcome to ACS730 Project! My private IP is $myip</h1><br>Built by Terraform!"  >  /var/www/html/index.html
+sudo chmod -R 777 /var/www
+sudo echo "<h1>Welcome to ACS730 Project! My private IP is $myip</h1><br>"  >  /var/www/html/index.html
+sudo echo '<h1>Team Member 1: Haojie Fu</h1><br>' >> /var/www/html/index.html
+sudo echo '<img src="https://acs730-project-images.s3.us-east-1.amazonaws.com/merry_christmas.jpg" />' >> /var/www/html/index.html
 sudo systemctl start httpd
 sudo systemctl enable httpd
